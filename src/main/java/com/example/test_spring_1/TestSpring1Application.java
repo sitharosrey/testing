0861bcc6686f9bc6570fce6d170cf9bd7a4e5ad0
SreySitharo <sitharosrey@gmail.com>
2023-11-11 15:12:18 +0700

    my first commit

---
+++ src/main/java/com/example/test_spring_1/TestSpring1Application.java
@@ -0,0 +1,13 @@
+package com.example.test_spring_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpring1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpring1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_spring_1/TestSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_spring_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpring1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
